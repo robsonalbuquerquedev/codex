b039057ecbee0b2cc959a93b5ea99b9fae094080
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-15 22:39:20 -0300

    feat: adicionar rota /games/retro-classicos/do-8bit-ao-ps2-revolucoes

---
+++ app/games/retro-classicos/do-8bit-ao-ps2-revolucoes/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function Do8bitAoPs2RevolucoesPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
