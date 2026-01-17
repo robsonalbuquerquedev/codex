ef655f48d87c6d1dfc8076e7cd4e8da5453b34d1
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-17 15:28:24 -0300

    feat: adicionar rota /historia-origem/influencias-referencias/literatura-e-cinema-como-mapa-de-inspiracao

---
+++ app/historia-origem/influencias-referencias/literatura-e-cinema-como-mapa-de-inspiracao/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function LiteraturaECinemaComoMapaDeInspiracaoPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
