94b081b4d1b1f82fe29234b60707184389e4e322
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-17 18:31:28 -0300

    feat: adicionar rota /curiosidades/bastidores-curiosos/mudancas-de-roteiro-que-transformaram-obras

---
+++ app/curiosidades/bastidores-curiosos/mudancas-de-roteiro-que-transformaram-obras/page.tsx
@@ -0,0 +1,7 @@
+import ArticleUnderConstruction from "@/components/shared/ArticleUnderConstruction";
+
+export default function MudancasDeRoteiroQueTransformaramObrasPage() {
+    return (
+        <ArticleUnderConstruction />
+    )
+}
